205c2c0f2b8fa39852101813e03e5e8a8d18c7c8
Bernhard Korn <bkorn@gmx.de>
2020-05-31 17:05:58 +0200

    som misc changes

---
+++ DXFLib/Basic/ExceptionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXFLib.Basic
+{
+    public class ExceptionHelper
+	{
+		public static string GetExceptionMessage(Exception exception)
+		{
+			if (exception == null)
+			{
+				throw new ArgumentNullException("exception");
+			}
+			string exceptionMessage = exception.Message;
+			while (exception.InnerException != null)
+			{
+				exceptionMessage = exceptionMessage + "\r\n" + Convert.ToString(exception.InnerException.Message);
+				exception = exception.InnerException;
+			}
+			return exceptionMessage;
+		}
+	}
+}
